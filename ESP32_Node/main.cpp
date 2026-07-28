5a5acacb092dce061432d3d578d6669c87e8d1d0
Voyrox <ewen@macculloch.net>
2026-07-28 14:17:57 +1000

    Update main.cpp

--- ESP32_Node/main.cpp
+++ ESP32_Node/main.cpp
@@ -1,1 +1,12 @@
-.
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("ESP32 Node is starting...");
+}
+
+void loop() {
+  // Your main code here
+  Serial.println("ESP32 Node is running...");
+  delay(1000); // Delay for 1 second
+}
