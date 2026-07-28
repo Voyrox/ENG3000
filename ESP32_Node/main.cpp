2d395cb88ae64524dec68f82a44d17ea4567f1c7
Voyrox <ewen@macculloch.net>
2026-07-28 15:32:16 +1000

    .

--- ESP32_Node/main.cpp
+++ ESP32_Node/main.cpp
@@ -5,6 +5,7 @@ constexpr char WIFI_SSID[] = "Group_1_ENGG3000";
 constexpr char WIFI_PASSWORD[] = "1234567890";
 constexpr char SERVER_IP[] = "192.168.1.100";
 constexpr uint16_t SERVER_PORT = 3000;
+int nodeID = -1;
 
 WiFiClient client;
 
@@ -19,6 +20,13 @@ void connectServer() {
 
     client.setNoDelay(true);
     Serial.println("TCP connected");
+    if (client.available()) {
+        String idStr = client.readStringUntil('\n');
+        nodeID = idStr.toInt();
+        Serial.print("Assigned ID: ");
+        Serial.println(nodeID);
+    }
+    Serial.println("ESP32 Node is initialized");
 }
 
 void connectWiFi() {
@@ -34,6 +42,16 @@ void connectWiFi() {
     Serial.print("Connected. IP: ");
     Serial.println(WiFi.localIP());
     connectServer();
+}
+
+void sendData(const String& data) {
+    if (client.connected()) {
+        client.println(data);
+        Serial.print("Sent: ");
+        Serial.println(data);
+    } else {
+        Serial.println("Not connected to server. Cannot send data.");
+    }
 }
 
 void setup() {
