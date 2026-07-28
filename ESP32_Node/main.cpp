73b6edb6b74e089e6df367fe4b4a94390d61c7ad
Voyrox <ewen@macculloch.net>
2026-07-28 15:03:04 +1000

    Update main.cpp

--- ESP32_Node/main.cpp
+++ ESP32_Node/main.cpp
@@ -1,12 +1,48 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+constexpr char WIFI_SSID[] = "Group_1_ENGG3000";
+constexpr char WIFI_PASSWORD[] = "1234567890";
+constexpr char SERVER_IP[] = "192.168.1.100";
+constexpr uint16_t SERVER_PORT = 3000;
+
+WiFiClient client;
+
+void connectServer() {
+    while (!client.connected()) {
+        Serial.println("Connecting to TCP server...");
+
+        if (!client.connect(SERVER_IP, SERVER_PORT)) {
+            delay(1000);
+        }
+    }
+
+    client.setNoDelay(true);
+    Serial.println("TCP connected");
+}
+
+void connectWiFi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.setSleep(false);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to Wi-Fi...");
+    }
+
+    Serial.print("Connected. IP: ");
+    Serial.println(WiFi.localIP());
+    connectServer();
+}
 
 void setup() {
   Serial.begin(115200);
   Serial.println("ESP32 Node is starting...");
+  connectWiFi();
 }
 
 void loop() {
-  // Your main code here
   Serial.println("ESP32 Node is running...");
   delay(1000); // Delay for 1 second
 }
